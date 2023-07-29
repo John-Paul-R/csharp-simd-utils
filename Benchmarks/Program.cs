daff0afb9c5ff66bf10f5ec1f0e879b77f301849
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-07-29 01:30:46 -0400

    fix build

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -4,6 +4,6 @@ using Benchmarks;
 // var bench = new Bench_SpanVsArray();
 // bench.GlobalSetup();
 // bench.FullSpan();
-BenchmarkRunner.Run<Bench_SpanVsArray>();
-// BenchmarkRunner.Run<Bench_Scratch>();
+// BenchmarkRunner.Run<Bench_SpanVsArray>();
+BenchmarkRunner.Run<Bench_Scratch>();
 // BenchmarkRunner.Run<Bench_FuncVsStruct>();
